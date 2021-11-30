05037513aa56a966f4af08540208364456fba1db
Levi Redlin <lredlin@wisc.edu>
2021-11-30 16:42:59 -0600

    Added dictionaries to access maps from numbers to strings

---
+++ result_parsing_python/dictionaries.py
@@ -0,0 +1,12 @@
+# Index is determiner number, value is determiner string
+determiners = {"jeb", "lom", "ked", "dap"}
+
+# Index is number, value is stem string
+big_stems = {"rooz", "mon", "pex", "frus", "glarm", "stam", "pav", "teep", "zout", "foud", "chag", "theb", "pum", "sarb", "plim", "naos", "vor", "skun"}
+
+# Index is number, value is stem string
+small_stems = {"kred", "hull", "biff", "bep", "zimb", "sib", "zeev", "tam", "toch", "heef", "burr", "chet"}
+
+# Index 0 -> singular, Index 1 -> plural
+big_endings = {"ok", "ool"}
+small_endings = {"esh", "aaf"}
