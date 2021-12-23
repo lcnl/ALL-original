6a9b29e0177145e687fdde872f63a4a3c9f0d840
Levi Redlin <lredlin@wisc.edu>
2021-12-23 14:07:13 -0800

    Added the rest of the necessary files/code

--- result_parsing_python/re_parsing_library.py
+++ result_parsing_python/re_parsing_library.py
@@ -1,10 +1,20 @@
 import re
 import numpy as np
 import dictionaries
+import csv
 
 # Global sets storing determiners and nouns for comparison 
 determiners = []
-nouns = []
+stems = []
+endings = []
+determiner_name_to_number = {}
+big_stems = []
+small_stems = []
+ending_mapping = {}
+det_to_size_and_plurality = {}
+levenshtein_threshold = 1
+big_stems_number_to_training_or_testing = {}
+small_stems_number_to_training_or_testing = {}
 
 def split_by_regex(response):
     split_string = re.split(" ",response)
@@ -74,14 +84,14 @@ def levenshtein(seq1, seq2):
                     matrix[x-1,y-1] + 1,
                     matrix[x,y-1] + 1
                 )
-    print (matrix)
     return (matrix[size_x - 1, size_y - 1])
 
-def morpheme_levenshtein_comparison(morpheme, threshold, is_determiner):
+# type: 0 -> determiner 1 -> stem 2 -> ending
+def morpheme_levenshtein_comparison(morpheme, threshold, type):
     # Stores the current lowest LD between the morpheme and the possible options
     curr_closest = 1000000
 
-    if(is_determiner):
+    if(type == 0):
         for i in determiners:
             if (levenshtein(morpheme, i) == curr_closest):
                 # Unmappable, 0 indicates that morpheme is not gibberish, needs human inspect
@@ -96,8 +106,23 @@ def morpheme_levenshtein_comparison(morpheme, threshold, is_determiner):
         
         # Mapped successfully, return 1 indicating success and the mapped morpheme
         return (1, curr_best)
-    else:
-        for i in nouns:
+    elif (type == 1):
+        for i in stems:
+            if (levenshtein(morpheme, i) == curr_closest):
+                # Unmappable, 0 indicates that morpheme is not gibberish, needs human inspect
+                return (0, morpheme)
+            if(levenshtein(morpheme, i) < threshold and levenshtein(morpheme, i) < curr_closest):
+                curr_closest = levenshtein(morpheme, i)
+                curr_best = i
+
+        # Mapped to nothing because every LD higher than threshold, -1 indicates gibberish
+        if (curr_closest == 1000000):
+            return (-1, morpheme)
+        
+        # Mapped successfully, return 1 indicating success and the mapped morpheme
+        return (1, curr_best)
+    elif (type == 2):
+        for i in endings:
             if (levenshtein(morpheme, i) == curr_closest):
                 # Unmappable, 0 indicates that morpheme is not gibberish, needs human inspect
                 return (0, morpheme)
@@ -134,5 +159,174 @@ def determiner_assignment(det_names):
         else:
             # Invalid input, bad news 
             return -1
-    return det_map
-print(determiner_assignment(["det1_big2p.wav","det2_small2p.wav","det3_big2s.wav","det4_small2s.wav"]))
+    global det_to_size_and_plurality
+    det_to_size_and_plurality = det_map
+
+def obtain_det_names(filename):
+    det_names = []
+    with open(filename) as csvfile:
+        row_reader = csv.reader(csvfile, delimiter=',')
+        for row in row_reader:
+            # Obtains training/testing designation for big and small neighborhoods
+            reg_data = re.findall("det[0-9]+_(?:(?:big)|(?:small))[0-9]+[ps]",row[2])
+            big_or_small = re.findall("(?:(?:big)|(?:small))",reg_data[0])[0]
+            monster_num = int(re.findall("[0-9]+",reg_data[0])[1])
+            if (big_or_small == 'big'):
+                big_stems_number_to_training_or_testing[monster_num] = row[5]
+            else:
+                small_stems_number_to_training_or_testing[monster_num] = row[5]
+            det_names.append(row[2])
+            det_names.append(row[3])
+    return det_names
+
+
+
+# Determines the mapping from names of determiners, stems and endings to their numbers, plurality, and 
+# neighborhood and updates global maps
+# IMPORTANT NOTE: filename must be a csv file, if not, change to csv in excel first then call this
+def det_stem_ending_mapping(filename):
+    with open(filename) as csvfile:
+        row_reader = csv.reader(csvfile, delimiter=',')
+        for row in row_reader:
+            # map determiners
+            if("det" in row[0]):
+                test_number = re.compile('det[1-4]')
+                if(test_number.match(row[0]) != None):
+                    find_number = re.compile("\d")
+                    determiner_name_to_number[row[1]] = int(find_number.findall(row[0])[0])
+                    determiners.append(row[1])
+            # map big neighborhood
+            if("big" in row[2]):
+                test_number = re.compile('big\d')
+                if(test_number.match(row[2]) != None):
+                    big_stems.append(row[3])
+                    stems.append(row[3])
+            # map small neighborhood
+            if("small" in row[4]):
+                test_number = re.compile('small\d')
+                if(test_number.match(row[4]) != None):
+                    small_stems.append(row[5])
+                    stems.append(row[5])
+            # map ending plurality
+            if("big" in row[7]):
+                ending_mapping[row[8]] = ('big', 's')
+                ending_mapping[row[9]] = ('big', 'p')
+                endings.append(row[8])
+                endings.append(row[9])
+            if("small" in row[7]):
+                ending_mapping[row[8]] = ('small', 's')
+                ending_mapping[row[9]] = ('small', 'p')
+                endings.append(row[8])
+                endings.append(row[9])
+                    
+            
+            
+
+# This function will return a 2x2 list of strings that is structured as the table appears in page 4 of the
+# "instructions and codebooks" document. The table will only have the first 2 rows filled out, as 
+# This is enough to extrapolate sufficient information to obtain the next 3 rows, which will be performed
+# in a different function
+def get_initial_table(filename):
+    table = []
+    with open(filename) as csvfile:
+        row_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in row_reader:
+            if('typed_response' in row[4]):
+                pass
+            else:
+                typed_response = row[4]
+                target_response = row[5]
+                # add row to table, add contents as first 2 entries to list
+                table.append([])
+                table[len(table)-1].append(typed_response)
+                table[len(table)-1].append(target_response)
+    return(table)
+
+def fill_out_table(initial_table):
+    for row in initial_table:
+        typed_response = split_by_regex(row[0])
+        # NOTE: this part of the code relies on the expected response to be properly split by regex, 
+        # in testing, this was always the case 
+        expected_response = split_by_regex(row[1])
+        
+        if(typed_response[0] == None):
+            row.append("couldn't")
+            row.append("parse")
+            row.append("syllables")
+        else:
+            
+
+        # Determiner analysis
+            correct_det = morpheme_levenshtein_comparison(expected_response[0], 1, 0)
+            det_analysis = morpheme_levenshtein_comparison(typed_response[0], 1, 0)
+            if(correct_det[0] == -1):
+                print("PROBLEM: correct value did not map, check levenshtein threshold")
+                exit(0)
+                pass
+            if(det_analysis[0] == -1):
+                row.append("unmappable: "+det_analysis[1])
+            elif(det_analysis[1] == correct_det[1]):
+                row.append("correct")
+            else:
+                row.append(det_to_size_and_plurality[determiner_name_to_number[det_analysis[1]]])
+        # Stem analysis
+            correct_stem = morpheme_levenshtein_comparison(expected_response[1], 1, 1)
+            stem_analysis = morpheme_levenshtein_comparison(typed_response[1], 1, 1)
+
+            # IMPORTANT NOTE: I had to change "burr" in language_pilot5 to "bur", it does not like that 
+            # the unconnected ending has 2 rs, and also that isn't really necessary
+            if(correct_stem[0] == -1):
+                print("PROBLEM: correct value did not map, check levenshtein threshold")
+                exit(0)
+                pass
+            if(stem_analysis[0] == -1):
+                row.append("unmappable: "+stem_analysis[1])
+            elif(stem_analysis[1] == correct_stem[1]):
+                row.append("correct")
+            else:
+                to_comp = stem_analysis[1]
+                if(to_comp in big_stems):
+                    monster_number = big_stems.index(to_comp)
+                    row.append("big "+big_stems_number_to_training_or_testing[monster_number])
+                else:
+                    monster_number = small_stems.index(to_comp)
+                    row.append("small "+small_stems_number_to_training_or_testing[monster_number])
+        # Ending analysis
+            correct_ending = morpheme_levenshtein_comparison(expected_response[2], 1, 2)
+            ending_analysis = morpheme_levenshtein_comparison(typed_response[2], 1, 2)
+            if(correct_ending[0] == -1):
+                print("PROBLEM: correct value did not map, check levenshtein threshold")
+                exit(0)
+                pass
+            if(ending_analysis[0] == -1):
+                row.append("unmappable: "+ending_analysis[1])
+            elif(ending_analysis[1] == correct_ending[1]):
+                row.append("correct")
+            else:
+                row.append(ending_mapping[ending_analysis[1]])
+    return initial_table
+
+        
+
+def retrieve_results(morpheme_map_file, setup_file, data_file, output_file):
+    det_stem_ending_mapping(morpheme_map_file)
+    det_names = obtain_det_names(setup_file)
+    determiner_assignment(det_names)
+    data_table = get_initial_table(data_file)
+    table = fill_out_table(data_table)
+    with open(output_file, 'w') as f: 
+        details = ["typed_response", "target_response", "typed_det", "typed_stem", "typed_ending"]
+        write = csv.writer(f) 
+        write.writerow(details) 
+        write.writerows(table) 
+
+#det_stem_ending_mapping("language_pilot5.csv")
+#det_names = obtain_det_names("s_71993_setup.csv")
+#determiner_assignment(det_names)
+#data_table = get_initial_table("production_test_data_pilot5.csv")
+#fill_out_table(data_table)
+morpheme_map_file = "language_pilot5.csv"
+setup_file = "s_71993_setup.csv"
+data_file = "production_test_data_pilot5.csv"
+output_file = "output.csv"
+retrieve_results(morpheme_map_file, setup_file, data_file, output_file)
